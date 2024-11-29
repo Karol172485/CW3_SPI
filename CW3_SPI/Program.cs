a10b4790ed95151f7517188e7e388aee78d71fd0
Karol <Karol@DESKTOP-11HA4SD>
2024-11-29 21:45:29 +0100

    Add project files.

---
+++ CW3_SPI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.ForegroundColor
+            = ConsoleColor.Yellow;
+
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.Clear();
+
+
+        Console.WriteLine("Hello, what's Your name?");
+
+        var name = Console.ReadLine();
+
+        var currentDate = DateTime.Now;
+
+        Console.WriteLine($"{Environment.NewLine} Hi {name}, the current time is: {currentDate:t}, on a {currentDate:d}\n  ~Karol Piotrowski, CW3");
+
+        Console.Write($"{Environment.NewLine} Press any button to exit the console view..");
+
+        Console.ReadKey(true);
+
+    }
+}
